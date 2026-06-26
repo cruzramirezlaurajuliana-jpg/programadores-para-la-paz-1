0f949f21ccf417a809e935ee393bb9364828472f
cruzramirezlaurajuliana-jpg <cruzramirezlaurajuliana@gmail.com>
2026-06-26 00:25:55 +0000

    Actividad semana 8

--- semana6/server.js
+++ semana6/server.js
@@ -4,25 +4,18 @@ const app = express();
 app.use(express.json());
 
 app.post('/registro', (req, res) => {
-    const nombre = req.body.nombre;
-    const mensaje = req.body.mensaje;
-    res.json({
-        estado: "Datos recibidos",
-        nombre: nombre,
-        mensaje: mensaje
-    });
-});
 
-app.post('/incidencia', (req, res) => {
-    const tipo = req.body.tipo;
-    const descripcion = req.body.descripcion;
-    res.json({
-        mensaje: "Incidencia registrada",
-        tipo: tipo,
-        descripcion: descripcion
-    });
+  const nombre = req.body.nombre;
+  const mensaje = req.body.mensaje;
+
+  res.json({
+    estado: "Datos recibidos",
+    nombre: nombre,
+    mensaje: mensaje
+  });
+
 });
 
 app.listen(3000, () => {
-    console.log('Servidor ejecutándose en puerto 3000');
+  console.log('Servidor ejecutándose en puerto 3000');
 });
